c18cf91e858d46c59b1a4222b673295472f9f687
jiwonp7747 <angry9908@gmail.com>
2025-12-11 10:53:13 +0900

    setting: common response, exception 추가

---
+++ common/exception/api_exception.py
@@ -0,0 +1,7 @@
+from common.response.code.base_code import BaseCode
+
+class ApiException(Exception):
+    def __init__(self, code: BaseCode, message: str = None):
+        self.code = code
+        self.message = message or code.message()
+        super().__init__(self.code)
---
+++ common/response/code/__init__.py
@@ -0,0 +1,9 @@
+from common.response.code.base_code import BaseCode
+from common.response.code.success_code import SuccessCode
+from common.response.code.failure_code import FailureCode
+
+__all__ = [
+    'FailureCode',
+    'SuccessCode',
+    'BaseCode',
+]
